0ecff76abb7cf3120f2725a7a6f03275e1426622
Harry Hackett <elderyrrah@gmail.com>
2015-02-22 21:09:21 +1300

    Changed testing loop

--- fav.js
+++ fav.js
@@ -3,7 +3,7 @@ var favorites = JSON.parse(localStorage.favorites);
 var tab = Options.add_tab('fav-tab','star',_("Favorites"));
 $("#favoriteOptionsBox").val(localStorage.favorites);
 
-for(i=0; i<favorites.length; i++){$("test").appendTo(tab.content)} //testing for loop
+for(i=0; i<favorites.length; i++){$("<p>test</p>").appendTo(tab.content)} //testing for loop
 
 var textarea = $("<textarea></textarea>").css({
   "font-size": 12,
