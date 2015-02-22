57602d6936379c38cf3f769381e17dce2e325327
Harry Hackett <elderyrrah@gmail.com>
2015-02-22 22:50:27 +1300

    Forcing output to figure out code

--- fav.js
+++ fav.js
@@ -17,6 +17,7 @@ function drop(ev) {
     ev.preventDefault();
 //    var data = $(ev.target).html();
     var data = ev.dataTransfer.getData("text");
+    alert(data);
     $(ev.target).append(data);
 }
 
