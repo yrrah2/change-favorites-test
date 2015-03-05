f4aed7613a4ca6fbf13793c0a50b69de8bd2ae26
Harry Hackett <elderyrrah@gmail.com>
2015-03-06 09:25:23 +1300

    Working (not well though) enter to add board script added

--- fav.js
+++ fav.js
@@ -66,13 +66,19 @@ var submit = $("<input id=\"submitFavorites\" onclick=\"document.location.reload
     left: 5, right: 5
 });
 
-$(plusBox).onkeydown = function(event){
-    event = event || window.event;
-    var keycode = event.charCode || event.keyCode;
-    if ( event.which == 13 ) {
-		$("#plus").click();
-	}
-}
+var test = document.getElementById('plusBox');
+
+test.onkeydown = function(e) {
+    if ( this.className === 'hold' ) { return false; }
+    this.className = 'hold';
+    if ( e.which == 13 ) {
+        addBoard();
+    }
+};
+
+test.onkeyup = function() {
+    this.className = '';
+};
 
 //Adding content to the tab
 $(tab.content).append(helpMessage); //Adding the help message to the tab
