5100d5ed62fe6cf09e4d9b834f5234e40b4aecd0
Harry Hackett <elderyrrah@gmail.com>
2015-03-09 15:41:09 +1300

    Update fav.js

--- fav.js
+++ fav.js
@@ -8,20 +8,20 @@ var favList = [];
 var minusList = [];
 var helpMessage = "<span>Drag the boards to sort them.</span><br></br>";
 
-//Setting functions
-var generateList = function(){
+//Creating functions
+function generateList(){
 	var favStor = [];
-    for(i=1; i<favorites.length+1; i++){
-        favStor.push($("#sortable > div:nth-child("+i+")").html());
-    }
+  for(i=1; i<favorites.length+1; i++){
+  	favStor.push($("#sortable > div:nth-child("+i+")").html());
+  }
 	return JSON.stringify(favStor);
-}; //This will generate a list of boards based off of the list on the screen
-var removeBoard = function(boardNumber){
+} //This will generate a list of boards based off of the list on the screen
+function removeBoard(boardNumber){
 	favorites.splice(boardNumber, 1);
 	localStorage.favorites = JSON.stringify(favorites);
 	$("#sortable > div:nth-child("+(boardNumber+1)+")").remove();
 	$("#minusList > div:nth-child("+(favorites.length+1)+")").remove();
-}; //This removed a board from favorites, localStorage.favorites and the page
+} //This removes a board from favorites, localStorage.favorites and the page
 function addBoard(){
 	$("#sortable").append("<div>"+($("#plusBox").val())+"</div>");
 	$("#minusList").append("<div onclick=\"removeBoard("+favorites.length+")\" style=\"cursor: pointer; margin-left: 5px\">-</div>");
@@ -42,7 +42,7 @@ function addBoard(){
 	$("#plusBox").focus(); //Moving cursor into text box again after refresh
 	$(plus).appendTo(tab.content); //Adding the plus to the tab
 	$(submit).appendTo(tab.content); //Adding button to the tab
-}
+} //This adds the text inside the textbox to favorites, localStorage.favorites and the page
 
 //Creating content
 
@@ -81,7 +81,7 @@ $(space).appendTo(tab.content);
 $(plusBox).appendTo(tab.content);
 $("#plusBox").keydown(function( event ) {
  if(event.keyCode == 13){
- 	addBoard();
+ 	$("#plus").click();
  }
 });
 document.getElementById("plusBox").value = "";
